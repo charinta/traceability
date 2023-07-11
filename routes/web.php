449981b5e2bfab433cdf83f19a5a34597ac55f4c
charinta <charintakantidewi@gmail.com>
2023-07-11 10:02:39 +0700

    route untuk dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+Route::get('/', [HomeController::class, 'home']);
+	Route::get('dashboard', function () {
+		return view('dashboard');
+	})->name('dashboard');
 });
+
+Route::get('/login', function () {
+    return view('session/login-session');
+})->name('login');
