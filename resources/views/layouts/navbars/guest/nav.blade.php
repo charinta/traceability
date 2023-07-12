9d49ae23d7bd7578a77aa29699e7c76d2ac924a6
Nursina Hamdalah <nursinahamdalah21@gmail.com>
2023-07-12 08:20:00 +0700

    navbar udah fix icon

--- resources/views/layouts/navbars/guest/nav.blade.php
+++ resources/views/layouts/navbars/guest/nav.blade.php
@@ -1,24 +1,30 @@
 <!-- Navbar -->
-<nav class="navbar-expand-lg position-absolute top-0 z-index-3 my-3 {{ Request::is('static-sign-up') ? 'w-100 navbar-transparent mt-4' : 'py-2 start-0 end-0 mx4' }}">
+
+
+<nav class="navbar navbar-expand-sm shadow-none navbar-fixed-top">
     <div class="container-fluid {{ Request::is('static-sign-up') ? 'container' : 'container-fluid' }}">
-        <a class="font-weight-bolder ms-lg-0 {{ Request::is('static-sign-up') ? 'text-white' : '' }}" href="{{ url('dashboard') }}">
+          <a class="font-weight-bolder navbar-brand">
             Dashboard
-        </a>
-        <div id="navigation" class="collapse navbar-collapse">
-            <ul class="navbar-nav">
+          </a>
+            <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+              <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                  <ul class="navbar-nav ">
+                <!-- icon profile -->
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="{{ url('profile') }}">
-                        <i class="fa fa-user opacity-6 me-1 {{ Request::is('static-sign-up') ? '' : 'text-dark' }}"></i>
+                    <a class="nav-link me-2" href="{{ auth()->user() ? url('#') : url('#') }}">
+                        <i class="fas fa-user-circle opacity-6 me-1 {{ Request::is('#') ? 'active' : '' }}"></i>
                     </a>
                 </li>
+                <!-- icon settings -->
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="{{ auth()->user() ? url('static-sign-up') : url('register') }}">
-                        <i class="fas fa-user-circle opacity-6 me-1 {{ Request::is('static-sign-up') ? '' : 'text-dark' }}"></i>
+                    <a class="nav-link me-2" href="{{ auth()->user() ? url('#') : url('#') }}">
+                        <i class="fas fa-cog opacity-6 me-1 {{ Request::is('#') ? 'active' : '' }}"></i>
                     </a>
                 </li>
+                <!-- icon notifications -->
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="{{ auth()->user() ? url('static-sign-in') : url('login') }}">
-                        <i class="fas fa-key opacity-6 me-1 {{ Request::is('static-sign-up') ? '' : 'text-dark' }}"></i>
+                    <a class="nav-link me-2" href="{{ auth()->user() ? url('#') : url('#') }}">
+                        <i class="fas fa-bell opacity-6 me-1 {{ Request::is('#') ? 'active' : ''}}"></i>
                     </a>
                 </li>
             </ul>
